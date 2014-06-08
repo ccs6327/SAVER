0ea0b8e85c0961442f74e293b4e23582c3809d78
ccs6327 <ccs6327@gmail.com>
2014-06-08 15:01:08 +0800

    homepage and user page with login bug

--- saver.py
+++ saver.py
@@ -2,17 +2,25 @@ import webapp2
 import jinja2
 import os
 
+from google.appengine.api import users
+
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/templates"))
 
-
-class MainPage(webapp2.RequestHandler):
-    """ Handler for the front page."""
+class UserPage(webapp2.RequestHandler):
+    """ Handler for the front page after user login."""
 
     def get(self):
-        template = jinja_environment.get_template('homepage.html')
-        self.response.out.write(template.render())
+        user = users.get_current_user()
+        if user: #signed in already
+            template_values = {
+                'user_mail': users.get_current_user().email(),
+                'logout': users.create_logout_url(self.request.host_url),
+            }
+            template = jinja_environment.get_template('userhomepage.html')
+            self.response.out.write(template.render())
+        else:
+            self.redirect(self.request.host_url)
 
-
-app = webapp2.WSGIApplication([('/', MainPage)],
+app = webapp2.WSGIApplication([('/user', UserPage)],
                               debug=True)
